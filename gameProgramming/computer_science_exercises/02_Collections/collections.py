818cbe455e844400cd5730ba9831bbab4e498314
da0603 <142345414+da0603@users.noreply.github.com>
2023-10-04 14:08:52 -0400

    v0.1a

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -1,1 +1,10 @@
-# Collections Examoles, Damian Arnold, v0.0 
+# Collections Examoles, Damian Arnold, v0.0
+
+# List -- ORDERED, CHANGEABLE, ALLOWS DUPLICATE VALUES
+breackfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
+# Each item in list is known as an ELEMENT.
+# The position in the list for each is the INDEX.
+# The element "BACON" is at index 0.
+#Python Only index -1 is the last item on the list>
+testScores = [95, 100, 25, 15, 27, 35]
+classGPA = [3.14, 2.5, 1.74, 1.99, 0.99, 4.25]
