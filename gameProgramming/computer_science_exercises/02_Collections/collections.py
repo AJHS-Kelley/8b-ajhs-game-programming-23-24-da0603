a9dad424da7387e527ae393f419d56b28ef823c1
da0603 <142345414+da0603@users.noreply.github.com>
2023-10-04 14:13:11 -0400

    v0.1b
    Printing Lists

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -1,4 +1,4 @@
-# Collections Examoles, Damian Arnold, v0.0
+# Collections Examoles, Damian Arnold, v0.1b
 
 # List -- ORDERED, CHANGEABLE, ALLOWS DUPLICATE VALUES
 breackfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
@@ -8,3 +8,8 @@ breackfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
 #Python Only index -1 is the last item on the list>
 testScores = [95, 100, 25, 15, 27, 35]
 classGPA = [3.14, 2.5, 1.74, 1.99, 0.99, 4.25]
+
+# Printing Contents of a List
+print(breackfastFoods)
+print(testScores)
+print(classGPA)
