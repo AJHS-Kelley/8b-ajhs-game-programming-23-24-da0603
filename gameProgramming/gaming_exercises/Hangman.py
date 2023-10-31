9e7bf87aa08d7e0807d6f10e1801a60bb4d79e4c
da0603 <142345414+da0603@users.noreply.github.com>
2023-10-31 14:51:23 -0400

    Hangman v0.0

---
+++ gameProgramming/gaming_exercises/Hangman.py
@@ -0,0 +1,55 @@
+# Hangman Game by Damian Arnold, v0.0
+
+words = 'dog1 sink2 tree3 cat4 ape5 take7 eat8 book9 look10 paper11 water12 router13 window14 apple15 artery16 femur17 tongue18 bathroom19 building20 uniformitarianism21 indubitably22 pronunciation23 definition24 Hippopotomonstrosesquippedaliophobia25 trypophobia26 phytoplankton27 pneumonoultramicroscopicsilicovolcanoconiosis28 pneumonia29 descriptian30'. split()
+HANGMAN_BOARD = [''    
+    +---+
+    0   |
+        |
+        |
+    ======''','''
+    +---+
+    0   |
+    |   |
+        |
+    =====''','''
+    +---+
+     0  |
+    /|  |
+        |
+    =====''','''
+    +---+
+     0  |
+    /|\ |
+        |
+    ======''','''
+    +---+
+     0   |
+    /|\  |
+    /    |
+    ======''','''
+    +---+
+     0  |
+    /|\ |
+    / \ |
+    =====''',''']
+
+def getRandomWord(wordlist): 
+    wordIndex = random.randint(0, len(wordList) - 1)
+    # Ien(listName) -1 is extremely common
+    return wordList[wordIndex] 
+
+def displayBoard(missedLetters, correctLetters, secretWord):
+    print(HANGMAN_BOARD[len(missedLetters)])
+    print()
+
+    print('Missed Letters:', end = ' ')
+    for eachLetter in missedLetters:
+        print(eachLetter , end = ' ')
+    print()
+
+
+#i = 0
+#while 1 < 50:
+    #word = getRandomWord(words)
+    #print(word)
+    #i += 1
