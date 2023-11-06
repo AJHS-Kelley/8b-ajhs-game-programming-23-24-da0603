badfa5540529b41fe87eb6101d9d955d09a1566f
da0603 <142345414+da0603@users.noreply.github.com>
2023-11-06 14:50:33 -0500

    Almost done

--- gameProgramming/gaming_exercises/Hangman.py
+++ gameProgramming/gaming_exercises/Hangman.py
@@ -1,47 +1,75 @@
 # Hangman Game by Damian Arnold, v0.0
+import random
+#words = 'dog1 sink2 tree3 cat4 ape5 take7 eat8 book9 look10 paper11 water12 router13 window14 apple15 artery16 femur17 tongue18 bathroom19 building20 uniformitarianism21 indubitably22 pronunciation23 definition24 Hippopotomonstrosesquippedaliophobia25 trypophobia26 phytoplankton27 pneumonoultramicroscopicsilicovolcanoconiosis28 pneumonia29 descriptian30'. split()
+#dictionary version
+#stored in key;value pairs
+#actual dictionary word (key) : value (definition)
+#uses {} to specify a dictionary
+words = {'Colors': 'red blue teal gold green white gray brown indigo pink silver'.split(),
+         'Animals': 'cat dog cow lion sheep lizaed aligator hippopotamus pig deer'.split(),
+         'Shapes': 'square rectangle triangle trapizoid rambus diamond pentagon hexagon octogon dodecehedron'.split(),
+         'Fruit': 'apple orange grape grapefuit dragonfruit bannana'.split()}
+         
 
-words = 'dog1 sink2 tree3 cat4 ape5 take7 eat8 book9 look10 paper11 water12 router13 window14 apple15 artery16 femur17 tongue18 bathroom19 building20 uniformitarianism21 indubitably22 pronunciation23 definition24 Hippopotomonstrosesquippedaliophobia25 trypophobia26 phytoplankton27 pneumonoultramicroscopicsilicovolcanoconiosis28 pneumonia29 descriptian30'. split()
-HANGMAN_BOARD = [''','''
+
+
+HANGMAN_BOARD = [''
     +---+
         |
         |
         |
-    =====''','''
+       =======''','''
     +---+
     0   |
         |
         |
-    ======''','''
+       =======''','''
     +---+
     0   |
     |   |
         |
-    =====''','''
+       ======''','''
     +---+
      0  |
     /|  |
         |
-    =====''','''
+       ======''','''
     +---+
      0  |
     /|\ |
         |
-    ======''','''
+       ======''','''
     +---+
      0   |
     /|\  |
     /    |
-    ======''','''
+       ======''','''
     +---+
      0  |
     /|\ |
     / \ |
-    =====''',''']
+       ======''','''
+    +---+
+     0  |
+    o-o |
+    / \ |
+       =====''','''
+    +---+
+     0  |
+    o-o |
+    / \ |
+   o   o|
+      =====''',''']
 
-def getRandomWord(wordlist): 
-    wordIndex = random.randint(0, len(wordList) - 1)
-    # Ien(listName) -1 is extremely common
-    return wordList[wordIndex] 
+# def getRandomWord(wordlist): 
+#     wordIndex = random.randint(0, len(wordList) - 1)
+#     # Ien(listName) -1 is extremely common
+#     return wordList[wordIndex] 
+
+def getRandomWord(wordDict): 
+    wordKey = random.choice(list(wordDict.keys()))
+    wordIndex = random.randint(0, len(wordDict[wordKey]) -1)
+    return [wordDict[wordKey][wordIndex], wordKey] 
 
 def displayBoard(missedLetters, correctLetters, secretWord):
     print(HANGMAN_BOARD[len(missedLetters)])
@@ -71,25 +99,43 @@ def getGuess(alreadyGuessed):
         guess = guess.lower()
         if len(guess) != 1:
             print('Please enter a single letter.')
-            elif guess in alreadyGuessed:
-                print('Letter has been guessed already, try again.')
-            elif guess not in 'abcdefghijklmnopqrstuvwxyz':
-                print('Please guess a Letter from the English alphabet')
-            else:
-                 return guess
+        elif guess in alreadyGuessed:
+             print('Letter has been guessed already, try again.')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('Please guess a Letter from the English alphabet')
+        else:
+            return guess
 
-def playAgain()
+def playAgain():
     print('Do you want to play again? Yes or no?')
     return input().lower().startswitch('y')
 
 
 print('Play Hangman')
+
+
+difficulty = 'X'
+while difficulty not in 'EMH':
+    print('Please choose Easy, Medium, or Hard. Type the first letter then press enter')
+    difficulty = input().upper()
+if difficulty == 'M':
+    del HANGMAN_BOARD[8]
+    del HANGMAN_BOARD[7]
+if difficulty == 'H':
+    del HANGMAN_BOARD[8]
+    del HANGMAN_BOARD[7]
+    del HANGMAN_BOARD[5]
+    del HANGMAN_BOARD[3]
+
+
+
 missedLetters =''
 correctLetters = ''
-secretWord = getRandomWord(words)
+secretWord,secretSet = getRandomWord(words)
 gameIsDone = False
 
 while True:
+    print('The secret word is from the ' + secretSet + 'category.\n')
     displayBoard(missedLetters, correctLetters, secretWord)
 
     guess = getGuess(missedLetters + correctLetters)
@@ -103,31 +149,29 @@ while True:
             if secretWord[i] not in correctLetters:
                 FoundAllLetters = False
                 break
-            if foundAllLetters:
-                print('Congrats! You did it!')
-                print('The secret word was' + secretWord)
-                gameIsDone = True
+        if foundAllLetters:
+            print('Congrats! You did it!')
+            print('The secret word was' + secretWord)
+            gameIsDone = True
     else:
         missedLetters = missedLetters + guess
 
         if len(missedLetters) == len(HANGMAN_BOARD) -1:
             displayBoard(missedLetters, correctLetters, secretWord)
             print('You have run out of guesses and lost the game.')
-            print('You made this number of correct guesses' = str(len(correctLetters)))
+            print('You made this number of correct letters' = str(len(correctLetters)))
             print('The secret word was' = secretWord)
-    gameIsDode = True
+            gameIsDode = True
+    if gameIsDone:
         if playAgain():
-            missedLetters =
-            correctLetters=
-            gameIsDone = FalsesecretWord = getRandomWord(words)
+            missedLetters = ''
+            correctLetters = ''
+            gameIsDone = False
+            secretWord,secretSet = getRandomWord(words)
         else:
             break
+    
             
 
             
 
-#i = 0
-#while 1 < 50:
-    #word = getRandomWord(words)
-    #print(word)
-    #i += 1
