69bec1b0687036e0b69239144c6d0fd8305267b3
da0603 <142345414+da0603@users.noreply.github.com>
2023-11-13 13:58:42 -0500

    Example Game Functions Project

--- gameProgramming/gaming_exercises/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/gaming_exercises/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,1 +1,14 @@
 # Example Game Functions Project Damian Arnold v.0.0
+import random
+
+def functionOne():
+    pass
+
+def functionTwo(param1):
+    pass
+
+def fuctionThree(param1 = "Default Value"):
+    pass
+
+def functionFour(param1,param2,param3):
+    pass
