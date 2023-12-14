6fe1a69cdd2cb6c8317528bf298960eeb7c1be89
da0603 <142345414+da0603@users.noreply.github.com>
2023-12-14 14:00:45 -0500

    tester

--- modules/tester.py
+++ modules/tester.py
@@ -1,5 +1,10 @@
 import dice
 
 
-dice.roll(1,6)
-dice.display(1,6)
+roll1 = dice.display(1,6)
+roll2 = dice.display(1,6)
+
+if dice.isDoubles(roll1, roll2):
+    print("You rolled a double.\n")
+else:
+    print("It was not a double.\n")
